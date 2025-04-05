2b19bf2d625b06cd2c5eca0beb20084403098240
hyeonju <shj040201@naver.com>
2025-04-05 14:34:58 +0900

    [#1] Add Project + CodeBaseSetting

---
+++ SOPT36_Week1/SOPT36_Week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT36_Week1
+//
+//  Created by 성현주 on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor  = .red
+    }
+
+
+}
+
