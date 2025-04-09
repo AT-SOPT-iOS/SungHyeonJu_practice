1ec4cae38fdb69992b20ebaec0109e7bf233f49b
hyeonju <shj040201@naver.com>
2025-04-10 01:21:38 +0900

    [#1] 옵셔널 메서드 수정

--- SOPT36_Week1/SOPT36_Week1/Extention/UIButton+.swift
+++ SOPT36_Week1/SOPT36_Week1/Extention/UIButton+.swift
@@ -15,12 +15,4 @@ extension UIButton {
         self.setTitleColor(titleColor, for: .normal)
         self.titleLabel?.font = fontName
     }
-
-    /// Button border 속성 지정
-    func setRoundBorder(borderColor: UIColor, borderWidth: CGFloat, cornerRadius: CGFloat) {
-        layer.masksToBounds = true
-        layer.borderColor = borderColor.cgColor
-        layer.borderWidth = borderWidth
-        layer.cornerRadius = cornerRadius
-    }
 }
