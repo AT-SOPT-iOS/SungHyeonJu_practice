bb6082e2030a2f13dd2323bc080751d0c0b7b7a0
hyeonju <shj040201@naver.com>
2025-04-10 00:19:24 +0900

    [#1] Pretendard Font 추가

---
+++ SOPT36_Week1/SOPT36_Week1/Extention/UIFont+.swift
@@ -0,0 +1,38 @@
+//
+//  UIFont+.swift
+//  SOPT36_Week1
+//
+//  Created by 성현주 on 4/10/25.
+//
+
+import UIKit
+
+extension UIFont {
+    class var head1: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 25)!
+    }
+
+    class var subhead1: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 18)!
+    }
+
+    class var subhead2: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 16)!
+    }
+
+    class var subhead3: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 15)!
+    }
+
+    class var subhead4: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 14)!
+    }
+
+    class var subhead5: UIFont {
+        return UIFont(name: "Pretendard-Bold", size: 14)!
+    }
+
+    class var info12: UIFont {
+        return UIFont(name: "Pretandard-Bold", size: 12)!
+    }
+}
--- SOPT36_Week1/SOPT36_Week1/LoginViewController.swift
+++ SOPT36_Week1/SOPT36_Week1/LoginViewController.swift
@@ -18,7 +18,7 @@ class LoginViewController: UIViewController {
         label.textColor = .black
         label.textAlignment = .center
         label.numberOfLines = 2
-        label.font = .boldSystemFont(ofSize: 16)
+        label.font = UIFont.subhead1
         return label
     }()
 
@@ -33,12 +33,12 @@ class LoginViewController: UIViewController {
     let passwordTextField: UITextField = {
         let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
         textField.placeholder = "비밀번호"
-        textField.font = .systemFont(ofSize: 14)
+        textField.font = UIFont(name: "Pretendard-Bold", size: 25.0)
         textField.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
         return textField
     }()
 
-    var loginButton: UIButton = {
+    lazy var loginButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
         button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
         button.setTitle("로그인하기", for: .normal)
@@ -100,8 +100,3 @@ class LoginViewController: UIViewController {
         self.present(welcomeViewController, animated: true)
     }
 }
-
-//MARK: - Preview
-#Preview("VC", traits: .defaultLayout, body: {
-    LoginViewController()
-})
