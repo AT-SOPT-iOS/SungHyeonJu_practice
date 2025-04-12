749b51f3bdc05adba87e9f98533ca0fd19c795e1
hyeonju <shj040201@naver.com>
2025-04-12 14:55:20 +0900

    [#3] DelegatePattenrVC 추가

---
+++ SOPT36_Week1/SOPT36_Week1/Feature/Delegate/LoginViewController_DelegatePattern.swift
@@ -0,0 +1,112 @@
+//
+//  LoginViewController_DelegatePattern.swift
+//  SOPT36_Week1
+//
+//  Created by 성현주 on 4/12/25.
+//
+
+import UIKit
+
+final class LoginViewController_DelegatePattern: UIViewController{
+
+
+    // MARK: - UIComponent
+    lazy var titleLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 53, y: 163, width: 236, height: 44))
+        label.text = "동네라서 가능한 모든것\n당근에서 가까운 이웃과 함께해요."
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = UIFont.subhead1
+        return label
+    }()
+
+    let idTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 276, width: 335, height: 52))
+        textField.placeholder = "아이디"
+        textField.font = UIFont.subhead4
+        textField.backgroundColor = .Gray200
+        textField.layer.cornerRadius = 3
+        textField.clearButtonMode = .always
+        textField.addLeftPadding(width: 23)
+        return textField
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 20, y: 335, width: 335, height: 52))
+        textField.placeholder = "비밀번호"
+        textField.font = UIFont.subhead4
+        textField.backgroundColor = .Gray200
+        textField.layer.cornerRadius = 3
+        textField.clearButtonMode = .always
+        textField.addLeftPadding(width: 23)
+        return textField
+    }()
+
+    lazy var loginButton: BaseFillButton = {
+        let button = BaseFillButton(frame: CGRect(x: 21, y: 422, width: 332, height: 58))
+        button.setTitle("로그인하기", for: .normal)
+        button.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubviews(titleLabel,
+                              idTextField,
+                              passwordTextField,
+                              loginButton)
+        configureTextFieldTargets()
+    }
+
+    //MARK: - ActionMethod
+    @objc
+    private func loginButtonDidTap() {
+        pushToWelcomeVC()
+        //presentWelcomeVC()
+    }
+
+    @objc
+    private func textFieldsDidChange() {
+        let isIDFilled = !(idTextField.text ?? "").isEmpty
+        let isPasswordFilled = !(passwordTextField.text ?? "").isEmpty
+        loginButton.isEnabled = isIDFilled && isPasswordFilled
+    }
+
+    //MARK: - PrivateMethod
+    private func configureTextFieldTargets() {
+        [idTextField, passwordTextField].forEach {
+            $0.addTarget(self, action: #selector(textFieldsDidChange), for: .editingChanged)
+        }
+    }
+
+    //MARK: - NavigationMethod
+
+    private func pushToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController_DelegatePattern()
+
+        //프로퍼티로 구현
+        //welcomeViewController.id = idTextField.text ?? ""
+
+        //함수로 구현
+        welcomeViewController.setLabelText(id: idTextField.text)
+        welcomeViewController.delegate = self
+        self.navigationController?.pushViewController(welcomeViewController, animated: true)
+    }
+
+    private func presentToWelcomeVC() {
+        let welcomeViewController = WelcomeViewController()
+        welcomeViewController.modalPresentationStyle = .formSheet
+        self.present(welcomeViewController, animated: true)
+    }
+}
+
+extension LoginViewController_DelegatePattern: DataBindDelegate {
+
+    func dataBind(id: String) {
+        idTextField.text = id
+    }
+}
---
+++ SOPT36_Week1/SOPT36_Week1/Feature/Delegate/WelcomeViewController_DelegatePattern.swift
@@ -0,0 +1,104 @@
+//
+//  WelcomeViewController_DelegatePattern.swift
+//  SOPT36_Week1
+//
+//  Created by 성현주 on 4/12/25.
+//
+
+import UIKit
+
+
+protocol DataBindDelegate: AnyObject {
+    func dataBind(id: String)
+}
+
+
+final class WelcomeViewController_DelegatePattern: UIViewController {
+
+    // MARK: - Properties
+    private var id: String?
+    weak var delegate : DataBindDelegate?
+
+    // MARK: - UIComponent
+    lazy var welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        label.font = UIFont.head1
+        return label
+    }()
+
+    lazy var raabitImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
+        imageView.image = ImageLiterals.rabbit
+        return imageView
+    }()
+
+    lazy var mainButton: BaseFillButton = {
+        let button = BaseFillButton(frame: CGRect(x: 20, y: 426, width: 332, height: 58))
+        button.setTitle("메인으로", for: .normal)
+
+        //button disable때문에 구현을위해 임의로 셀렉터함수 변경했습니다
+        button.addTarget(self, action: #selector(reLoginButtonDidTapped), for: .touchUpInside)
+        return button
+    }()
+
+    lazy var reLoginButton: BaseFillButton = {
+        let button = BaseFillButton(frame: CGRect(x: 20, y: 498, width: 332, height: 58))
+        button.setTitle("다시 로그인", for: .normal)
+        button.addTarget(self, action: #selector(reLoginButtonDidTapped), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.view.addSubviews(welcomeLabel,
+                              mainButton,
+                              reLoginButton,
+                              raabitImageView)
+        bindID()
+    }
+
+    //MARK: - ActionMethod
+    @objc
+    private func reLoginButtonDidTapped() {
+        if let id = id {
+            delegate?.dataBind(id: id)
+            print(id)
+        }
+
+        if self.navigationController == nil {
+            self.dismiss(animated: true)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+
+    @objc
+    private func mainButtonDidTapped() {
+        print("메인으로!")
+    }
+
+    //MARK: - PrivateMethod
+
+    private func bindID() {
+        guard let id = id, !id.isEmpty else {
+            self.welcomeLabel.text = "???님\n반가워요!"
+            return
+        }
+        self.welcomeLabel.text = "\(id)님\n반가워요!"
+    }
+
+    //MARK: - HelperMethod
+
+
+    public func setLabelText(id: String?){
+            self.id = id
+        }
+}
+
+
