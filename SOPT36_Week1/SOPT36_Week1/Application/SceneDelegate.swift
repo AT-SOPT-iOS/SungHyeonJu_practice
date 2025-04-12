5be301e9c17ade1e06801cfc2d4fab7567442ee1
hyeonju <shj040201@naver.com>
2025-04-12 14:55:20 +0900

    [#3] DelegatePattenrVC 추가

--- SOPT36_Week1/SOPT36_Week1/Application/SceneDelegate.swift
+++ SOPT36_Week1/SOPT36_Week1/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        let loginVC = LoginViewController()
+        let loginVC = LoginViewController_DelegatePattern()
         let navController = UINavigationController(rootViewController: loginVC)
         window.rootViewController = navController
         self.window = window
